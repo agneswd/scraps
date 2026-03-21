ae0d72826ddc88bd832299f4259da20a4edcd457
agneswd <agneswdmail@gmail.com>
2026-03-21 17:20:46 +0100

    build: add vite-plugin-mkcert for local https testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
 import path from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
   server: {
+    host: true,
     proxy: {
       '/api': {
         target: 'http://localhost:8090',
